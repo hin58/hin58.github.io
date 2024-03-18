8c9602fe90a3872a2f7f30f7745affcdd94bdaab
viml <vimalkg@gmail.com>
2024-03-18 16:39:31 +0530

    deleted app.vue components

--- src/kontact.vue
+++ src/kontact.vue
@@ -1,12 +1,12 @@
 <template>
     <table>
-        <tr><td>email:</td><td>vimalkg@gmail.com</td></tr>
-        <tr><td>mobile</td><td>7291098286</td></tr>
-        <tr><td>fesbuk</td><td><a href="https://fb.com/ztrabc">fesbuk</a></td></tr>
-        <tr><td>tvitter:</td><td><a href="https://twitter.com/etphor">tvitter/etphor(84)</a></td></tr>
-        <tr><td>fesbuk:</td><td><a href="https://fb.com/8fgrm">fb.com/8fgrm</a></td></tr>
-        <tr><td>fesbuk:</td><td><a href="https://fb.com/hin584">fb.com/hin584</a></td></tr>
-        <tr><td>fesbuk:</td><td><a href="https://fb.com/Tmil58">fb.com/Tmil58</a></td></tr>
-        <tr><td>email:</td><td>vimalkg@gmail.com</td></tr>
+        <tr><td>eमaiल:</td><td>vimalkg@gmail.com</td></tr>
+        <tr><td>coनटacट</td><td>7291098286</td></tr>
+        <tr><td>फaceबooक</td><td><a href="https://fb.com/ztrabc">fesbuk</a></td></tr>
+        <tr><td>टviटटeर:</td><td><a href="https://twitter.com/etphor">tvitter/etphor(84)</a></td></tr>
+        <tr><td>फeसबuक:</td><td><a href="https://fb.com/8fgrm">fb.com/8fgrm</a></td></tr>
+        <tr><td>फaceबooक:</td><td><a href="https://fb.com/hin584">fb.com/hin584</a></td></tr>
+        <tr><td>फeसबuक:</td><td><a href="https://fb.com/Tmil58">fb.com/Tmil58</a></td></tr>
+        <tr><td>eमaiल:</td><td>vimalkg@gmail.com</td></tr>
     </table>
 </template>

9c0867c90039ecfb3a98b69667b7a1b6afd3a03d
viml <vimalkg@gmail.com>
2024-03-18 16:48:36 +0530

    edited contact vue

--- src/kontact.vue
+++ src/kontact.vue
@@ -1,12 +1,11 @@
 <template>
     <table>
-        <tr><td>eमaiल:</td><td>vimalkg@gmail.com</td></tr>
+        <tr><td>eमaiल:</td><td><a href="mailto:vimalkg@gmail.com">वiमaल कuमaर</a></td></tr>
         <tr><td>coनटacट</td><td>7291098286</td></tr>
-        <tr><td>फaceबooक</td><td><a href="https://fb.com/ztrabc">fesbuk</a></td></tr>
-        <tr><td>टviटटeर:</td><td><a href="https://twitter.com/etphor">tvitter/etphor(84)</a></td></tr>
-        <tr><td>फeसबuक:</td><td><a href="https://fb.com/8fgrm">fb.com/8fgrm</a></td></tr>
-        <tr><td>फaceबooक:</td><td><a href="https://fb.com/hin584">fb.com/hin584</a></td></tr>
-        <tr><td>फeसबuक:</td><td><a href="https://fb.com/Tmil58">fb.com/Tmil58</a></td></tr>
-        <tr><td>eमaiल:</td><td>vimalkg@gmail.com</td></tr>
+        <tr><td>फaceबooक</td><td><a href="https://fb.com/ztrabc">zटरaबc</a></td></tr>
+        <tr><td>टviटटeर:</td><td><a href="https://twitter.com/etphor">टviटटeर/etphor(84)</a></td></tr>
+        <tr><td>फeसबuक:</td><td><a href="https://fb.com/8fgrm">8 फiनgeर मअतhस</a></td></tr>
+        <tr><td>फaceबooक:</td><td><a href="https://fb.com/hin584">हiन58(8aiueohcg)</a></td></tr>
+        <tr><td>फeसबuक:</td><td><a href="https://fb.com/Tmil58">தமiல58(8aiueohcg)</a></td></tr>
     </table>
 </template>

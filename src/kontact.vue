bcb9abb436f899d2a4995648cba2f32bd402c939
viml <vimalkg@gmail.com>
2024-03-18 13:26:19 +0530

    pushing vite buildpreview

---
+++ src/kontact.vue
@@ -0,0 +1,12 @@
+<template>
+    <table>
+        <tr><td>email:</td><td>vimalkg@gmail.com</td></tr>
+        <tr><td>mobile</td><td>7291098286</td></tr>
+        <tr><td>fesbuk</td><td><a href="https://fb.com/ztrabc">fesbuk</a></td></tr>
+        <tr><td>tvitter:</td><td><a href="https://twitter.com/etphor">tvitter/etphor(84)</a></td></tr>
+        <tr><td>fesbuk:</td><td><a href="https://fb.com/8fgrm">fb.com/8fgrm</a></td></tr>
+        <tr><td>fesbuk:</td><td><a href="https://fb.com/hin584">fb.com/hin584</a></td></tr>
+        <tr><td>fesbuk:</td><td><a href="https://fb.com/Tmil58">fb.com/Tmil58</a></td></tr>
+        <tr><td>email:</td><td>vimalkg@gmail.com</td></tr>
+    </table>
+</template>
